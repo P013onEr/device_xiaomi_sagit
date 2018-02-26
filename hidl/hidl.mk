1f9e160c1f5bfee67f1dd7a62538e86e9edc62c4
verevka <verevka86@gmail.com>
2018-02-26 23:04:20 +0300

    sagit: add custom power hal (reverted from commit fbbfb3db77a94f6f1c57dc72314c751d817787cd)

--- hidl/hidl.mk
+++ hidl/hidl.mk
@@ -87,7 +87,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service.sagit
+    android.hardware.power@1.1-service-qti
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
