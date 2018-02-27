438812c2aa21f9b754158678470e47e3ba2299c4
verevka <verevka86@gmail.com>
2018-02-27 08:25:48 +0300

    sagit: use old power hal

--- device.mk
+++ device.mk
@@ -280,6 +280,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
 
+# Power
+PRODUCT_PACKAGES += \
+    power.msm8998
+
 # Powerhint
 PRODUCT_COPY_FILES += $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
